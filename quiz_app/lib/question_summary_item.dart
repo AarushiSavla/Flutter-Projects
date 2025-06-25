d0e47855b8240e5378951f3550c293482450432b
Aarushi Savla <aarusavla@gmail.com>
2025-06-25 23:04:09 +0530

    changed ui for results screen

--- quiz_app/lib/question_summary_item.dart
+++ quiz_app/lib/question_summary_item.dart
@@ -1,41 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_summary.dart';
+
 //class extends statless
-class QuestionsSummaryItem extends StatelessWidget{
+class QuestionsSummaryItem extends StatelessWidget {
+  //constructor accepting summary data
+  QuestionsSummaryItem({
+    super.key,
+    required this.questionIndex,
+    required this.question,
+    required this.userAnswer,
+    required this.correctAnswer,
+  });
 
-//constructor accepting summary data 
-QuestionsSummaryItem({super.key, required this.questionIndex, required this.question, required this.userAnswer, required this.correctAnswer });
+  int questionIndex;
+  String question;
+  String userAnswer;
+  String correctAnswer;
 
- int questionIndex;
-String question;
-String userAnswer;
-String correctAnswer;
+  @override
+  Widget build(BuildContext context) {
+    // variable to check if the user answer is correct
+    var isCorrect = userAnswer == correctAnswer;
 
-@override
-Widget build(BuildContext context){
+    return Row(
+      children: [
+        //circle question index
+        Container(
+          //width height box alignment box decoration - shape and color
+          width: 20,
+          height: 20,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color:
+                isCorrect
+                    ? const Color.fromARGB(255, 255, 234, 216)
+                    : const Color.fromARGB(255, 232, 152, 138),
+          ),
+          child: Text(
+            (questionIndex + 1).toString(),
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
 
-// variable to check if the user answer is correct 
- var isCorrect = userAnswer==correctAnswer ;
+        const SizedBox(height: 30),
 
-return Row(
-  children: [
-    //circle question index
-    Container(
-      //width height box alignment box decoration - shape and color
-      width: 20,
-      height: 20,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: isCorrect?const Color.fromARGB(255, 211, 202, 121):const Color.fromARGB(255, 166, 44, 44),
-      ),
-    )
-
-  ],
-);
-
-
-}
-
-
+        //column which contains the question , user answer in purple and correct answer in yellow
+        Expanded(
+          child: Column(
+            children: [
+              Text(
+                question,
+                style: TextStyle(color: Colors.white, fontSize: 20),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                userAnswer,
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 42, 20, 88),
+                  fontSize: 15,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                correctAnswer,
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 255, 234, 216),
+                  fontSize: 15,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 }
